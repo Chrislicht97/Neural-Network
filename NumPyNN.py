dbcb324ad0ca4cb7848cb544ee21cc223b39e5cd
Chrislicht97 <clicht97@gmail.com>
2022-12-17 16:47:12 -0800

    Added second and third prompt for feedback loop as fLoop and fLoops for amount of loops.

--- NumPyNN.py
+++ NumPyNN.py
@@ -4,9 +4,10 @@ import random
 #empty list
 list1 = list() 
 sizes = ("small", "medium", "large")
+yesno = ("yes", "no")
 
 #Choices are small, medium, or large.
-size = input("Do you want small, medium, or large network?")
+size = input("Do you want small, medium, or large network?").lower()
 if size in sizes:
     inputs = 2 if size == "small" else 3 if size == "medium" else 4
 else:
@@ -19,21 +20,21 @@ for i in range(int(inputs)):
     #Adding that value to the list
     list1.append(int(n))
 
+fLoop = input("Do you want a feedback loop? Y/N").lower()
+if fLoop == "yes" or "y":
+    fLoops = input("How many loops would you like?")
+elif fLoop == "no" or "n":
+    pass
+else:
+    print("Sorry, that's not a valid input.")
+
 
 X = list1
-#X = np.random.randint(1, size=(1,2))
-#X = random.randint(0, 1)
+#X += np.random.randint(1, size=(1,2))
+#X += random.randint(0, 1)
 
 #(Weights fall between -1 and 1)
 weights = random.uniform(-1, 1)
 
-class Layer_Dense:
-    def __init__(self, n_inputs, n_neurons):
-        self.weights = weights * np.random.randn(n_inputs, n_neurons)
-        self.biases = np.zeros((1, n_neurons))
-    def forward(self, inputs):
-        self.output = np.dot(inputs, self.weights) + self.biases
-
-#output = np.dot(inputs, np.array(weights).T) + biases
 output = np.dot(X, weights)
 print(output)
